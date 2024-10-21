2439bfaaa4ff12df6b7bbbfa71e5559741f31fbb
sekne18 <sekne50@gmail.com>
2024-10-21 23:38:09 +0200

    init

---
+++ FdF/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jans <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 23:32:23 by jans              #+#    #+#             */
+/*   Updated: 2024/10/21 23:36:40 by jans             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	main(int argc, char **argv)
+{
+	/*
+		Validate the input and store it into an array.
+		Use get_next_line and strjoin for sotring inputs
+		then if successful -> fdf()
+		fdf() will draw everything
+	*/
+	if (argc < 2)
+		return (0);
+		
+	return (0);
+}
