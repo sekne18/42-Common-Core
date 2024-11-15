8c970e1f44b3396a4105a2d34a7876a2fe3327ba
sekne18 <sekne50@gmail.com>
2024-11-15 16:14:24 +0100

    finished

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,11 +6,25 @@
 /*   By: jans <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:05:38 by jans              #+#    #+#             */
-/*   Updated: 2024/10/31 17:21:04 by jans             ###   ########.fr       */
+/*   Updated: 2024/11/10 15:47:32 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	response_handler(int signum)
+{// Makes sure that client/server wait for each other
+	static int	received = 0;
+
+	if (signum == SIGUSR1)
+		received++;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
 
 void	send_char(int pid, char c)
 {
@@ -19,14 +33,13 @@ void	send_char(int pid, char c)
 
 	i = 8;
 	temp_char = c;
-	while (i-- > 0)
+	while (i--)
 	{
-		temp_char = c >> i;
-		if (temp_char % 2 == 0)
+		if (temp_char >> i & 1) //Shift right by i bits, and compares if equals to 1
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		usleep(30);
+		usleep(100);
 	}
 }
 
@@ -35,8 +48,14 @@ void	send_string(int pid, const char *str)
 	size_t	i;
 
 	i = 0;
-	while (i <= ft_strlen(str))
+	while (i < ft_strlen(str))
 		send_char(pid, str[i++]);
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -54,6 +73,10 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Invalid server PID\n", 1);
 		return (1);
 	}
+	signal(SIGUSR1, response_handler);
+	signal(SIGUSR2, response_handler);
 	send_string(pid, argv[2]);
+	//while (1)
+	//	pause();
 	return (0);
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,38 +6,52 @@
 /*   By: jans <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 09:43:31 by jans              #+#    #+#             */
-/*   Updated: 2024/11/04 07:32:30 by jans             ###   ########.fr       */
+/*   Updated: 2024/11/10 15:24:09 by jans             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handle_sigusr(int sig)
-{
-	static unsigned char	current_data;
-	static int				bit_position;
 
-	current_data |= (sig == SIGUSR1);
-	bit_position++;
-	if (bit_position == 8)
+void	handle_sigusr(int signum, siginfo_t *info, void *context)
+{
+	static	char	current_data = 0;
+	static	int		bit_position = 0;
+	static	pid_t	client_pid = 0;
+
+	(void)context;
+	if (!client_pid) // If no client has been assigned
+		client_pid = info->si_pid; // assigns the sending process's PID
+	current_data |= (signum == SIGUSR2); // Is condition is true, sets 1, otherwise 0
+	if (++bit_position == 8)
 	{
-		if (current_data == '\0')
-			ft_putchar_fd('\n', 1);
-		else
-			ft_putchar_fd(current_data, 1);
-		current_data = 0;
-		bit_position = 0;
+		bit_position = 0; // reset
+		if (!current_data)
+		{
+			kill(client_pid, SIGUSR2); // Indicates the end of messages
+			client_pid = 0; // reset
+			return ;
+		}
+		ft_putchar_fd(current_data, 1); //print character
+		current_data = 0; // reset
+		// signals back that it has successfully received and printed a character
+		kill(client_pid, SIGUSR1); 
 	}
 	else
-		current_data <<= 1;
+		current_data <<= 1; // shift one bit to the left, prep to receive the next bit
 }
 
 int	main(void)
 {
+	struct sigaction	sa;
+
 	ft_putstr_fd(ft_itoa(getpid()), 1);
 	ft_putchar_fd('\n', 1);
-	signal(SIGUSR1, handle_sigusr);
-	signal(SIGUSR2, handle_sigusr);
+	// Will pass extra info (the info and context) to the handler
+	sa.sa_flags = SA_SIGINFO; 
+	sa.sa_sigaction = &handle_sigusr;
+	sigaction(SIGUSR1, &sa, NULL); // Assign handler
+	sigaction(SIGUSR2, &sa, NULL); // Assign handler
 	while (1)
 		pause();
 	return (0);
