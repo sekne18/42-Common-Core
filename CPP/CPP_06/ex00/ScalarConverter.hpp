56e7a6776355577f4267e05f120861f5ef2297af
Jan Sekne <sekne50@gmail.com>
2025-02-25 11:57:16 +0100

    cpp 06 added

---
+++ CPP/CPP_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+public:
+    static void convert(const std::string& str);
+    
+
+};
+
+#endif // SCALARCONVERTER_HPP
