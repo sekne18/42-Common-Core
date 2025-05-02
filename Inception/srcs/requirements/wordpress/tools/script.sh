59b258ee3919572edf8704f426bc7db45f07dd92
jsekne <jsekne@vbox.guest.dpgmedia.net>
2025-05-02 07:25:10 +0000

    added redis

--- Inception/srcs/requirements/wordpress/tools/script.sh
+++ Inception/srcs/requirements/wordpress/tools/script.sh
@@ -14,6 +14,26 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
     wp user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PWD --allow-root
 
+    wp config set FORCE_SSL_ADMIN false --type=constant --allow-root
+
+    wp config  set WP_REDIS_HOST $REDIS_HOST --type=constant --allow-root
+
+    wp config set WP_REDIS_PORT $REDIS_PORT --type=constant --allow-root
+
+    wp config  set WP_CACHE true --type=constant --allow-root
+
+    until nc -z "$REDIS_HOST" 6379; do
+      echo "Waiting for Redis..."
+      sleep 2
+    done
+    wp plugin install redis-cache --allow-root
+
+    wp plugin activate redis-cache --allow-root
+
+
+
+    wp redis enable --allow-root
+
     chmod 777 /var/www/html/wp-content
 
     # install theme
